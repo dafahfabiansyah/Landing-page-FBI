149e95c0ab7b4005b172fbbbc5785f71ecde4d04
dafahfabiansyah <davarobiansyah@gmail.com>
2024-02-21 08:59:33 +0700

    add images in location section and pages

--- src/data/LocationData.ts
+++ src/data/LocationData.ts
@@ -1,51 +1,59 @@
+import FBI_BSD from "../../public/FBI - Plant Fresh Beton BSD.png";
+import FBI_Cianjur from "../../public/FBI - Plant Fresh Beton Cianjur.png";
+import FBI_Cikembar from "../../public/FBI - Plant Fresh Beton Cikembar.png";
+import FBI_Meruya from "../../public/FBI - Plant Fresh Beton Meruya.png";
+import FBI_Sentul from "../../public/FBI - Plant Fresh Beton Sentul.png";
+import FBI_Sukabumi from "../../public/FBI - Plant Fresh Beton Sukabumi.png";
+import FBI_Balaraja from "../../public/FBI - Plant Fresh Beton Balaraja.png";
+
 export const LocationData = [
     {
         id:1,
         title: "Plant fresh beton indonesia meruya",
         urlAddress: "https://maps.app.goo.gl/rJsPZvPRmJiZRG9EA",
-        image: '',
+        image: FBI_Meruya,
         address: "Jl. Raya Meruya No. 72, RT. 003/RW. 004, Meruya Utara., Kec. Kembangan, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta",
     },
     {
         id:2,
         title: "Plant fresh beton indonesia sentul",
         urlAddress: "https://maps.app.goo.gl/iFVuUgRLSHJDbVHZA",
-        image: '',
+        image: FBI_Sentul,
         address: "Jl. Bina Marga No. 76, RT. 003/RW. 005, Desa Senja, Kec. Citereup, Kabupaten Bogor",
     },
     {
         id:3,
         title: "Plant fresh beton indonesia cianjur",
         urlAddress: "https://maps.app.goo.gl/sgDQjxCM8pjwk6Su9",
-        image: '',
+        image: FBI_Cianjur,
         address: "Jl. Lingkar Timur No.43211, Sukamaju, Kec.Cianjur, Kab.Cianjur jawa barat - 43111",
     },
     {
         id:4,
         title: "Plant fresh beton indonesia sukabumi",
         urlAddress: "https://maps.app.goo.gl/2mS36XmAXm9r495x9",
-        image: '',
+        image: FBI_Sukabumi,
         address: "Jl. Asep Seafudin, KP. Cijurey Sawo, RT.006/RW. 003 Neglasari, Kec. Nyalindung, Kabupaten Sukabumi, Jawa Barat 43196",
     },
     {
         id:5,
         title: "Plant fresh beton indonesia serpong",
         urlAddress: "https://maps.app.goo.gl/Kscp3gXwjYqVCwwN6",
-        image: '',
+        image: FBI_BSD,
         address: "Jl. Otong Enjos, Ciater, Kec. Serpong, Kota Tangerang Selatan, Banten 15310",
     },
     {
         id:6,
         title: "Plant fresh beton indonesia cikembar",
-        urlAddress: "https://maps.app.goo.gl/Kscp3gXwjYqVCwwN6",
-        image: '',
+        urlAddress: "https://maps.app.goo.gl/pRvPjA6dA3Bdmt2V6",
+        image: FBI_Cikembar,
         address: "Jl. Raya Cikembang RT. 002/RW. 004 Neglasari, Kec. Nyalindung, Kabupaten Sukabumi, Jawa Barat 43196",
     },
     {
         id:7,
         title: "Plant fresh beton indonesia balaraja",
         urlAddress: "https://maps.app.goo.gl/aHYqqucLp19eh6go8",
-        image: '',
+        image: FBI_Balaraja,
         address: "Kampung Cogreg RT. 003/RW. 003 Desa Pasir Bolang, Kec. Tigaraksa, Kabupaten Tangerang, Banten",
     },
 ]as const
